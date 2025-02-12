dfec9eb764d25af43a3c5af81659efc4d4af3fc7
AbedElmajid <aamaged16@gmail.com>
2025-02-12 09:58:03 +0300

    UpdateRequest_ByManager,HR andView EmployeeManager

--- Attendance/Models/Interfaces/IAttendance.cs
+++ Attendance/Models/Interfaces/IAttendance.cs
@@ -12,6 +12,7 @@ namespace Attendance.Models.Interfaces
         IHttpActionResult Create(T model);
         IHttpActionResult Delete(int id);
         IHttpActionResult GetAll(int id);
+        IHttpActionResult GetAllRequestByManager(int id);
         IHttpActionResult GetById(int id);
         IHttpActionResult Update(int id, T model);
     }
