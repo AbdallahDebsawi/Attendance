1cb696bc76a5c7c5dcc6a953f0d31a9fc36cd0a1
AbdallahDebsawi <96738825+AbdallahDebsawi@users.noreply.github.com>
2025-02-18 09:39:15 +0300

    view leave type when updating a request

--- Attendance/Angular/src/app/components/request/request-entry/request-entry.component.ts
+++ Attendance/Angular/src/app/components/request/request-entry/request-entry.component.ts
@@ -10,18 +10,15 @@ enum AbsenceType {
   AnnualLeave = 1,
   SickLeave = 2,
   PersonalLeave = 3,
-  Other = 4
+  Other = 4,
 }
-
-
 
 @Component({
   selector: 'app-request-entry',
   templateUrl: './request-entry.component.html',
-  styleUrls: ['./request-entry.component.css']
+  styleUrls: ['./request-entry.component.css'],
 })
 export class RequestEntryComponent implements OnInit {
-
   requestList: Request[] = [];
   dataSource = new MatTableDataSource<Request>();
   requestForm: FormGroup;
@@ -29,12 +26,12 @@ export class RequestEntryComponent implements OnInit {
     { value: AbsenceType.AnnualLeave, label: 'Annual Leave' },
     { value: AbsenceType.SickLeave, label: 'Sick Leave' },
     { value: AbsenceType.PersonalLeave, label: 'Personal Leave' },
-    { value: AbsenceType.Other, label: 'Other' }
+    { value: AbsenceType.Other, label: 'Other' },
   ];
   status = [
     { value: Status.Pending, label: 'Pending' },
     { value: Status.Approved, label: 'Approved' },
-    { value: Status.Rejected, label: 'Rejected' }
+    { value: Status.Rejected, label: 'Rejected' },
   ];
   fileName: string = '';
   selectedFile: File | null = null;
@@ -64,41 +61,66 @@ export class RequestEntryComponent implements OnInit {
 
     this.requestForm = this.fb.group({
       id: [this.data?.Id || null],
-      typeOfAbsence: [{ 
-        value: this.data?.TypeOfAbsence || '', 
-        disabled: this.data?.Id ? (this.isManager || this.isHr) : false 
-      }, Validators.required],
+      typeOfAbsence: [
+        {
+          value:
+            this.absenceTypes[
+              this.data?.TypeOfAbsence ? this.data?.TypeOfAbsence - 1 : 0
+            ].value || '',
+          disabled: this.data?.Id ? this.isManager || this.isHr : false,
+        },
+        Validators.required,
+      ],
 
-      from: [{ 
-        value: this.formatDate(this.data?.From), 
-        disabled: this.data?.Id ? (this.isManager || this.isHr) : false 
-      }, Validators.required],
+      from: [
+        {
+          value: this.formatDate(this.data?.From),
+          disabled: this.data?.Id ? this.isManager || this.isHr : false,
+        },
+        Validators.required,
+      ],
 
-      to: [{ 
-        value: this.formatDate(this.data?.To), 
-        disabled: this.data?.Id ? (this.isManager || this.isHr) : false 
-      }, Validators.required],
+      to: [
+        {
+          value: this.formatDate(this.data?.To),
+          disabled: this.data?.Id ? this.isManager || this.isHr : false,
+        },
+        Validators.required,
+      ],
 
-      reasonOfAbsence: [{ 
-        value: this.data?.ReasonOfAbsence || '', 
-        disabled: this.data?.Id ? (this.isManager || this.isHr) : false 
-      }, Validators.required],
+      reasonOfAbsence: [
+        {
+          value: this.data?.ReasonOfAbsence || '',
+          disabled: this.data?.Id ? this.isManager || this.isHr : false,
+        },
+        Validators.required,
+      ],
 
-      managerStatus: [{ 
-        value: this.data?.ManagerStatus ? Number(this.data.ManagerStatus) : Status.Pending, 
-        disabled: this.data?.Id ? !this.isManager : false 
-      }],
-    
-      hrStatus: [{ 
-        value: this.data?.HRStatus ? Number(this.data.HRStatus) : Status.Pending, 
-        disabled: this.data?.Id ? !this.isHr : false 
-      }],
-      name: [{ 
-        value: this.data?.Name || 'Unknown',  
-        disabled: !!this.data?.Id 
-      }],
+      managerStatus: [
+        {
+          value: this.data?.ManagerStatus
+            ? Number(this.data.ManagerStatus)
+            : Status.Pending,
+          disabled: this.data?.Id ? !this.isManager : false,
+        },
+      ],
 
-      userId: [loggedInEmployee?.Id || null]
+      hrStatus: [
+        {
+          value: this.data?.HRStatus
+            ? Number(this.data.HRStatus)
+            : Status.Pending,
+          disabled: this.data?.Id ? !this.isHr : false,
+        },
+      ],
+      name: [
+        {
+          value: this.data?.Name || 'Unknown',
+          disabled: !!this.data?.Id,
+        },
+      ],
+
+      userId: [loggedInEmployee?.Id || null],
     });
 
     if (!this.data?.Id) {
@@ -110,6 +132,7 @@ export class RequestEntryComponent implements OnInit {
   minToDate: Date | null = null;
 
   ngOnInit(): void {
+    console.log('Request type:', this.absenceTypes[0].value);
     // this.requestForm = this.fb.group({
     //   from: ['', Validators.required],
     //   to: ['', Validators.required],
@@ -127,37 +150,41 @@ export class RequestEntryComponent implements OnInit {
   formatDate(date: any): string | null {
     if (!date) return null;
     const parsedDate = new Date(date);
-    return isNaN(parsedDate.getTime()) ? null : parsedDate.toISOString().split('T')[0];
+    return isNaN(parsedDate.getTime())
+      ? null
+      : parsedDate.toISOString().split('T')[0];
   }
 
   dateRangeValidator(formGroup: FormGroup) {
     const from = formGroup.get('from')?.value;
     const to = formGroup.get('to')?.value;
 
-    return from && to && new Date(from) > new Date(to) ? { invalidDateRange: true } : null;
+    return from && to && new Date(from) > new Date(to)
+      ? { invalidDateRange: true }
+      : null;
   }
 
   saveRequest(): void {
     if (this.requestForm.invalid) {
       return;
     }
-  
+
     let requestData = { ...this.requestForm.getRawValue() };
-  
-    
-  
-    requestData.from = requestData.from ? this.formatDate(requestData.from) : null;
+
+    requestData.from = requestData.from
+      ? this.formatDate(requestData.from)
+      : null;
     requestData.to = requestData.to ? this.formatDate(requestData.to) : null;
-  
+
     if (this.data?.Id) {
       requestData.name = this.data.Name;
       requestData.userId = this.data.UserId;
     }
-  
+
     const apiCall = requestData.id
       ? this.apiUrl.putData(`UpdateRequest/${requestData.id}`, requestData)
       : this.apiUrl.postData('CreateRequest', requestData);
-  
+
     apiCall.subscribe(
       (response) => {
         console.log('Request saved successfully:', response);
@@ -170,8 +197,6 @@ export class RequestEntryComponent implements OnInit {
       }
     );
   }
-  
-  
 
   onFileSelected(event: any) {
     const file = event.target.files[0];
