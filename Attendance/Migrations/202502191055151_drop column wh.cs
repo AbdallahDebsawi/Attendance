d96a6d48c624e3d0c8e08eb86ef10b2f99e3fdb4
Amer Absi <Amerabsiiiiii@gmail.com>
2025-02-19 15:16:06 +0300

    sqlExpress

---
+++ Attendance/Migrations/202502191055151_drop column wh.cs
@@ -0,0 +1,18 @@
+﻿namespace Attendance.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class dropcolumnwh : DbMigration
+    {
+        public override void Up()
+        {
+            DropColumn("dbo.AttendanceUsers", "WorkingHours");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.AttendanceUsers", "WorkingHours", c => c.Double());
+        }
+    }
+}
---
+++ Attendance/Migrations/202502191056479_addWorkingHoursColumn.cs
@@ -0,0 +1,18 @@
+﻿namespace Attendance.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addWorkingHoursColumn : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.AttendanceUsers", "WorkingHours", c => c.Double());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.AttendanceUsers", "WorkingHours");
+        }
+    }
+}
